5988081de964efb9cfc921dd94462830ecdb5f6e
Tia Wicaksono <wicaksono.tia@gmail.com>
2025-02-21 11:59:28 +0700

    bottomsheet

--- lib/pages/product/product.dart
+++ lib/pages/product/product.dart
@@ -1,9 +1,13 @@
+import 'package:esjerukkadiri/commons/containers/box_container.dart';
+import 'package:esjerukkadiri/commons/currency.dart';
+import 'package:esjerukkadiri/commons/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:esjerukkadiri/commons/colors.dart';
 import 'package:esjerukkadiri/controllers/product_controller.dart';
 import 'package:esjerukkadiri/pages/product/product_grid_view.dart';
 import 'package:esjerukkadiri/pages/product/product_list_view.dart';
+import 'package:badges/badges.dart' as badges;
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -22,78 +26,157 @@ class ProductPage extends StatelessWidget {
             flexibleSpace: FlexibleSpaceBar(
                 collapseMode: CollapseMode.parallax,
                 background: Image(
-                  image: AssetImage('assets/images/no_image.jpg'),
+                  image: AssetImage('assets/images/header.jpeg'),
                   fit: BoxFit.cover,
                 )),
             pinned: true,
-            expandedHeight: 200,
+            expandedHeight: 170,
             collapsedHeight: 35,
             toolbarHeight: 30,
           ),
           SliverPersistentHeader(
             delegate: _SliverAppBarDelegate(
-              Container(
-                color: MyColors.primary.withOpacity(0.7),
+              BoxContainer(
+                shadow: true,
+                radius: 0,
                 padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Obx(
-                    () =>
-                        // Container(
-                        //           decoration: BoxDecoration(
-                        //             color:
-                        //                 Colors.blue, // Background color of the button
-                        //             borderRadius:
-                        //                 BorderRadius.circular(8), // Rounded corners
-                        //           ),
-                        //           child: Row(
-                        //             mainAxisSize: MainAxisSize.min,
-                        //             children: [
-                        //               IconButton(
-                        //                 onPressed: () {
-                        //                   productController.toggleShowListGrid();
-                        //                 },
-                        //                 icon: Icon((productController
-                        //                         .showListGrid.value)
-                        //                     ? Icons.grid_view
-                        //                     : Icons.format_list_bulleted_outlined),
-                        //                 color: Colors.white,
-                        //               ),
-                        //               Text(
-                        //                 (productController.showListGrid.value)
-                        //                     ? 'Grid View'
-                        //                     : 'List View',
-                        //                 style: const TextStyle(
-                        //                   color: Colors.white,
-                        //                   fontSize: 16,
-                        //                 ),
-                        //               ),
-                        //             ],
-                        //           ),
-                        //         )
-                        Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            productController.toggleShowListGrid();
-                          },
-                          icon: Icon((productController.showListGrid.value)
-                              ? Icons.grid_view
-                              : Icons.format_list_bulleted_outlined),
-                          color: Colors.white,
+                child: Obx(
+                  () => Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Get.bottomSheet(
+                            Container(
+                              height: Get.height,
+                              child: Material(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(top: 5),
+                                      height: 5,
+                                      width: Get.width / 3,
+                                      decoration: BoxDecoration(
+                                        color: Colors.grey[300],
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.all(16),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(20),
+                                          topRight: Radius.circular(20),
+                                        ),
+                                      ),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            'Tia Wicaksono',
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(height: 10),
+                                          Text(
+                                            'This is a bottom sheet example.',
+                                            style: TextStyle(fontSize: 16),
+                                          ),
+                                          SizedBox(height: 20),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              Get.back();
+                                            },
+                                            child: Text('Close'),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          children: [
+                            const badges.Badge(
+                              badgeContent: Text(
+                                '2',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              badgeAnimation: badges.BadgeAnimation.fade(
+                                  animationDuration:
+                                      Duration(milliseconds: 400)),
+                              child: Icon(
+                                Icons.shopping_bag,
+                                color: MyColors.green,
+                                size: 30,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            const Text(
+                              'Rp',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: MySizes.fontSizeMd,
+                                color: MyColors.primary,
+                              ),
+                            ),
+                            Text(
+                              CurrencyFormat.convertToIdr(1234567, 0),
+                              style: const TextStyle(
+                                fontSize: MySizes.fontSizeXl,
+                                fontWeight: FontWeight.bold,
+                                color: MyColors.primary,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          (productController.showListGrid.value)
-                              ? 'Grid View'
-                              : 'List View',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.save),
+                        color: MyColors.green,
+                      ),
+                      // Text(
+                      //   'Save',
+                      //   style: const TextStyle(
+                      //     color: Colors.white,
+                      //     fontSize: 16,
+                      //   ),
+                      // ),
+                      IconButton(
+                        onPressed: () {
+                          productController.toggleShowListGrid();
+                        },
+                        icon: Icon((productController.showListGrid.value)
+                            ? Icons.grid_view_rounded
+                            : Icons.format_list_bulleted_rounded),
+                        color: MyColors.primary,
+                      ),
+                      // Text(
+                      //   (productController.showListGrid.value)
+                      //       ? 'Grid View'
+                      //       : 'List View',
+                      //   style: const TextStyle(
+                      //     color: Colors.white,
+                      //     fontSize: 16,
+                      //   ),
+                      // ),
+                    ],
                   ),
                 ),
               ),
